140eed52c72d0c68c4842b98db59d520486b458e
Eslam Hesham <esl2mhesham@gmail.com>
2020-11-04 07:22:04 +0200

    initialize app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="p-4 bg-red-300"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
