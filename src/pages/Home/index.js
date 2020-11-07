187a439f87b9f2f5ca94d077437a24d61b2e1c3b
eslamheshamm <esl2mhesham@gmail.com>
2020-11-07 19:00:27 +0200

    adding home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,234 @@
+import React from "react";
+
+export const Home = () => {
+	return (
+		<section>
+			<div class="w-10/12 mx-auto my-24">
+				<h1 class="text-6xl font-semibold tracking-wider">
+					Time to shine
+					<span class="text-5xl font-light block">Exceptional Performance</span>
+				</h1>
+				<p class="my-6">Grow your bussiness and boost up sale.</p>
+				<div class="flex items-center">
+					<input
+						class="appearance-none bg-transparent border-2 text-gray-700 p-4 w-2/5 md:w-2/5 leading-tight focus:outline-none"
+						type="text"
+						placeholder="Enter your mail here"
+					/>
+					<button class="p-4 bg-red-800 text-white" type="button">
+						Free Assesment
+					</button>
+				</div>
+
+				<ul class="flex my-32 items-center">
+					<li>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/1200px-Discord_logo.svg.png"
+							alt="Logo"
+							class="w-32 mx-4"
+						/>
+					</li>
+					<li>
+						<img
+							src="https://cdn.freebiesupply.com/images/large/2x/google-logo-transparent.png"
+							alt="Logo"
+							class="w-32 mx-4"
+						/>
+					</li>
+					<li>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Elaraby-group-logo.png/1200px-Elaraby-group-logo.png"
+							alt="Logo"
+							class="w-32 mx-4"
+						/>
+					</li>
+				</ul>
+			</div>
+
+			<div class="w-10/12 mx-auto">
+				<div class="text-center">
+					<p class="uppercase text-red-800 text-2xl tracking-wider font-normal">
+						Who we are
+					</p>
+					<h2 class="text-3xl font-medium my-2">We define the strategy</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
+						est deserunt suscipit ad illo maiores!
+					</p>
+				</div>
+				<div class="my-12 grid grid-cols-1 sm:grid-cols-3 gap-8">
+					<div class="p-8 bg-gray-100">
+						<div class="tracking-wider pt-32 pb-8">
+							<img
+								src="https://www.pinclipart.com/picdir/big/183-1832217_business-analysis-light-bulb-idea-vector-clipart.png"
+								alt="analysis"
+								class="py-8 w-32 h-48"
+							/>
+							<h2 class="text-4xl font-semibold my-8">Our Mission</h2>
+
+							<a href="#" class="text-2xl text-red-800 hover:underline">
+								See More
+							</a>
+						</div>
+					</div>
+					<div class="p-8 card">
+						<div class="tracking-wider pt-32 pb-8">
+							<img
+								src="https://cdn.onlinewebfonts.com/svg/img_462266.png"
+								alt="analysis"
+								class="py-8 w-32 h-48"
+							/>
+							<h2 class="text-4xl font-semibold my-8">Our Strategy</h2>
+
+							<a href="#" class="text-2xl text-red-800 hover:underline">
+								See More
+							</a>
+						</div>
+					</div>
+					<div class="p-8 border-2 border-solid border-red-800">
+						<div class="tracking-wider pt-16 pb-8">
+							<img
+								src="https://cdn2.iconfinder.com/data/icons/picons-essentials/71/document-512.png"
+								alt="analysis"
+								class="py-8 w-32"
+							/>
+							<h2 class="text-2xl font-semibold my-3">Our Vision</h2>
+							<p class="pb-8">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+								animi reprehenderit, minus eveniet quo aut architecto
+								dignissimos, deleniti aspernatur laboriosam odit explicabo
+								nihil, dicta iste.
+							</p>
+							<a href="#" class="text-2xl text-red-800 hover:underline">
+								See More
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="text-center my-32">
+					<p class="uppercase text-red-800 text-2xl tracking-wider font-normal">
+						Services
+					</p>
+					<h2 class="text-3xl font-medium my-2">Who should use our service</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
+						est deserunt suscipit ad illo maiores!
+					</p>
+				</div>
+			</div>
+			<div>
+				<div class="text-center bg-gray-100 py-32">
+					<img
+						src="https://thinkingportfolio.com/wp-content/uploads/2015/10/02G69046.jpg"
+						alt="meeting"
+						class="sm:w-1/2 w-10/12 mx-auto"
+					/>
+					<h2 class="text-3xl font-medium mt-8 mb-6">We define the strategy</h2>
+					<p class="my-4 sm:w-1/2 w-10/12 mx-auto">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+						ad quisquam quidem.
+					</p>
+					<p class="sm:w-1/2 w-10/12 mx-auto my-8">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+						optio perferendis inventore officia suscipit incidunt dolores
+						distinctio illo beatae ratione possimus, blanditiis dicta ullam
+						delectus totam voluptate repudiandae id voluptatibus.
+					</p>
+					<a href="#" class="text-1xl text-red-800 hover:underline mt-12">
+						See More Informaion
+					</a>
+				</div>
+				<div class="text-center py-32">
+					<img
+						src="https://www.targettraining.eu/wp-content/uploads/2019/02/meetings-practice.jpg"
+						alt="meeting"
+						class="sm:w-1/2 w-10/12 mx-auto"
+					/>
+					<h2 class="text-3xl font-medium mt-8 mb-6">Leader</h2>
+					<p class="my-4 sm:w-1/2 w-10/12 mx-auto">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+						modi molestiae, alias nesciunt quas impedit iusto tempora odio
+						facilis iste.
+					</p>
+					<p class="sm:w-1/2 w-10/12 mx-auto my-8">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+						optio perferendis inventore officia suscipit incidunt dolores
+						distinctio illo beatae ratione possimus, blanditiis dicta ullam
+						delectus totam voluptate repudiandae id voluptatibus.
+					</p>
+					<a href="#" class="text-1xl text-red-800 hover:underline mt-12">
+						See More Informaion
+					</a>
+				</div>
+				<div class="text-center my-32">
+					<h2 class="text-3xl font-medium my-8">Why Stay Strong</h2>
+					<p class="sm:w-1/2 w-10/12 mx-auto">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+						tempora officia, recusandae pariatur quisquam perferendis modi
+						magnam dolore animi, quibusdam a iste ad optio commodi, suscipit
+						aperiam praesentium dolores exercitationem?
+					</p>
+				</div>
+			</div>
+			<div class="my-12 grid grid-cols-1 sm:grid-cols-3 gap-8 mx-auto w-11/12">
+				<div class="px-6 py-12 border-solid border-2">
+					<img
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Globe_icon.svg/1200px-Globe_icon.svg.png"
+						alt="world"
+						class="w-32 py-16"
+					/>
+					<h2 class="text-3xl font-mediam my-6">Intarnational Standards</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+						quibusdam ullam esse architecto aperiam!
+					</p>
+				</div>
+				<div class="px-6 py-12 border-solid border-2">
+					<img
+						src="https://cdn.onlinewebfonts.com/svg/img_374433.png"
+						alt="world"
+						class="w-32 py-12"
+					/>
+					<h2 class="text-3xl font-mediam my-6">60 year of expreince</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+						quibusdam ullam esse architecto aperiam!
+					</p>
+				</div>
+				<div class="px-6 py-12 border-solid border-2">
+					<img
+						src="https://www.freeiconspng.com/thumbs/industry-icon-png/industry-icon-png-20.png"
+						alt="world"
+						class="w-32 py-16"
+					/>
+					<h2 class="text-3xl font-mediam my-6">Different Industries</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+						quibusdam ullam esse architecto aperiam!
+					</p>
+				</div>
+			</div>
+			<div>
+				<div class="text-center tracking-wider py-24 mx-auto bg-gray-100">
+					<p class="uppercase text-red-800 text-2xl tracking-wider font-semibold">
+						Contact us
+					</p>
+					<h2 class="sm:text-4xl text-3xl font-medium my-6">
+						We would love to hear from you
+					</h2>
+					<p>We are all excited to work with you and see your growing</p>
+					<div class="flex items-center justify-center my-12">
+						<input
+							class="appearance-none bg-transparent border-2 text-gray-700 px-4 w-3/5 md:w-2/5 py-4 leading-tight focus:outline-none"
+							type="text"
+							placeholder="Enter your mail here"
+						/>
+						<button class="p-4 bg-red-800 text-white" type="button">
+							Free Assesment
+						</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
