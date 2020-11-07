550b3cbb49c5546b5e5d0ac3632bc3aa297530f4
eslamheshamm <esl2mhesham@gmail.com>
2020-11-07 19:00:07 +0200

    creating footer

---
+++ src/screens/components/Footer/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<div className="w-10/12 mx-auto grid grid-cols-3 my-16">
+			<div className=" col-start-1 col-gap-2">
+				<h2 className="uppercase font-bold mb-6">StayStrong</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde nihil ad
+					ipsa dolores repudiandae illum ullam voluptates, eligendi provident
+					officia laboriosam ut asperiores amet, commodi quaerat itaque,
+					explicabo est laborum.
+				</p>
+				<p className="my-12">hossam.nabil@stay-strong.co</p>
+			</div>
+			<div className="flex flex-wrap justify-around  col-start-2 col-end-4 ">
+				<ul className="p-4">
+					<li className="text-2xl mb-6">Quick Links</li>
+					<li className="py-2">
+						<a href="#">How strong work?</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Security</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Report bug</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Pricing</a>
+					</li>
+				</ul>
+				<ul className="p-4">
+					<li className="text-2xl mb-6">About us</li>
+					<li className="py-2">
+						<a href="#">About strong</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Job</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Team</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Blog</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Testmonials</a>
+					</li>
+				</ul>
+				<ul className="p-4">
+					<li className="text-2xl mb-6">About us</li>
+					<li className="py-2">
+						<a href="#">About strong</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Job</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Team</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Blog</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Testmonials</a>
+					</li>
+				</ul>
+				<ul className="p-4">
+					<li className="text-2xl mb-6">About us</li>
+					<li className="py-2">
+						<a href="#">About strong</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Job</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Team</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Blog</a>
+					</li>
+					<li className="py-2">
+						<a href="#">Testmonials</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
